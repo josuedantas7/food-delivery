04a3d1d8eec98184f973e9e1fb10944ebf59a767
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-05-02 19:53:01 -0300

    feat: new utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
+function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+function formatedPrice(price: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(price)
+}
+
+function formatedDate(date: string) {
+  return new Intl.DateTimeFormat("pt-BR").format(new Date(date))
+}
+
+export { cn, formatedPrice, formatedDate}
